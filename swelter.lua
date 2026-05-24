f707c079410e064cfb2c020b2fe3bc3927148e2e
om.i.god neo <omneo@omigods-MacBook.local>
2026-05-24 15:21:50 -0500

    feat: page-cycled encoder + key UI

--- swelter.lua
+++ swelter.lua
@@ -28,6 +28,18 @@ local PARAMS = {
   {"hiss",          "hiss",          controlspec.new(0, 1,   'lin', 0, 0.0),  "hiss"},
 }
 
+-- e2/e3 = depth macros; e2s/e3s = the rate params reached by holding K1
+local PAGES = {
+  {name="REFRACTION",   e2="wow",     e3="flutter",  e2s="wow_rate",  e3s="flutter_rate"},
+  {name="HAZE/MIRAGE",  e2="haze",    e3="mirage",   e2s="haze_rate", e3s="mirage_detune"},
+  {name="TAPE",         e2="drive",   e3="age",      e2s="hiss",      e3s=nil},
+  {name="WEAR",         e2="dropout", e3="out_trim", e2s=nil,         e3s=nil},
+}
+local page = 1
+local shift = false
+local bypass = false
+local activity = 0
+
 local function add_params()
   params:add_separator("swelter")
   for _, p in ipairs(PARAMS) do
@@ -50,4 +62,31 @@ function redraw()
   screen.move(64, 36)
   screen.text_center("swelter")
   screen.update()
+end
+
+function enc(n, d)
+  activity = util.time()
+  if n == 1 then
+    params:delta("drywet", d)
+  else
+    local p = PAGES[page]
+    local id
+    if n == 2 then id = shift and p.e2s or p.e2
+    else            id = shift and p.e3s or p.e3 end
+    if id then params:delta(id, d) end
+  end
+  redraw()
+end
+
+function key(n, z)
+  activity = util.time()
+  if n == 1 then
+    shift = (z == 1)
+  elseif n == 2 then
+    if z == 1 then page = (page % #PAGES) + 1 end
+  elseif n == 3 then
+    bypass = (z == 1)
+    engine.drywet(bypass and 0 or params:get("drywet"))
+  end
+  redraw()
 end
